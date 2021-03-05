8f841c1df49e1d44f01c27d124b891e2e9822967
dannygarfield <dgarfield25@gmail.com>
2021-03-05 14:53:14 -0500

    EntryRow from class --> function

--- src/EntryRow.js
+++ src/EntryRow.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { formatDate } from './helpers';
 
-class EntryRow extends React.Component {
-  render() {
-    const entry = this.props.entry
-    const formattedAmount = (entry.Amount / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-    const formattedDate = formatDate(entry.EntryDate);
-    return (
-      <tr>
-        <td>{formattedDate}</td>
-        <td>{formattedAmount}</td>
-        <td>{entry.Category}</td>
-        <td>{entry.Description}</td>
-      </tr>
-    );
-  }
+function EntryRow(props) {
+  const entry = props.entry;
+  const formattedAmount = (entry.Amount / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  const formattedDate = formatDate(entry.EntryDate);
+  return (
+    <tr>
+      <td>{formattedDate}</td>
+      <td>{formattedAmount}</td>
+      <td>{entry.Category}</td>
+      <td>{entry.Description}</td>
+    </tr>
+  );
 }
+
+// class EntryRow extends React.Component {
+//   render() {
+//     const entry = this.props.entry;
+//     const formattedAmount = (entry.Amount / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+//     const formattedDate = formatDate(entry.EntryDate);
+//     return (
+//       <tr>
+//         <td>{formattedDate}</td>
+//         <td>{formattedAmount}</td>
+//         <td>{entry.Category}</td>
+//         <td>{entry.Description}</td>
+//       </tr>
+//     );
+//   }
+// }
 
 export default EntryRow;
